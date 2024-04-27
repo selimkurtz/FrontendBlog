57bd4c04aa0e5d8577b880562c78d0294ba50486
Selim KURT <77545975+selimkurtz@users.noreply.github.com>
2024-04-27 03:05:58 +0300

    First

---
+++ src/constants/images.js
@@ -0,0 +1,19 @@
+import Logo from "../assets/Logo.svg";
+import heroImage from "../assets/heroImage.svg";
+import post1 from "../assets/posts/post1.jpg";
+import post2 from "../assets/posts/post2.png";
+import post3 from "../assets/posts/post3.png";
+import postProfile from "../assets/posts/postprofile.svg";
+import post6 from "../assets/posts/post6.jpg";
+
+const images = {
+  Logo,
+  heroImage,
+  post1,
+  post2,
+  post3,
+  postProfile,
+  post6,
+};
+
+export default images;
