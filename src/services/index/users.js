78003f0702407b184b2c111dd524ea19d65935de
selimkurtz <selimkurt2222@gmail.com>
2024-05-09 03:18:45 +0300

    ProfilePictureUploads

--- src/services/index/users.js
+++ src/services/index/users.js
@@ -48,3 +48,48 @@ export const getUserProfile = async ({ token }) => {
     throw new Error(error.message);
   }
 };
+
+export const updateProfile = async ({ token, userData }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      "/api/users/updateProfile",
+      userData,
+      config
+    );
+
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const updateProfilePicture = async ({ token, formData }) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      "/api/users/updateProfilePicture",
+      formData,
+      config
+    );
+
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
