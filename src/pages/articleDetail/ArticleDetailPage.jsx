57fa29fcd7e7d91a443300b20c1120e8f3836e14
selimkurtz <selimkurt2222@gmail.com>
2024-05-11 18:21:24 +0300

    .

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -1,96 +1,140 @@
-import React from "react";
+/* eslint-disable no-empty-pattern */
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { generateHTML } from "@tiptap/html";
+import { Link, useParams } from "react-router-dom";
+import Bold from "@tiptap/extension-bold";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import Italic from "@tiptap/extension-italic";
+import parse from "html-react-parser";
+
 import MainLayout from "../../components/MainLayout";
 import BreadCrumbs from "../../components/BreadCrumbs";
-import { images } from "../../constants";
-import { Link } from "react-router-dom";
+import { images, stables } from "../../constants";
 import SuggestedPosts from "./container/SuggestedPosts";
 import CommentsContainer from "../../components/comments/CommentsContainer";
 import SocialShareButtons from "../../components/SocialShareButtons";
+import { getSinglePost } from "../../services/index/posts";
+import ArticleDetailSkeleton from "./components/ArticleDetailSkeleton";
+import ErrorMessage from "../../components/ErrorMessage";
+import { useSelector } from "react-redux";
+
+const postsData = [
+  {
+    _id: "1",
+    image: images.post2,
+    title: "Help children get better education",
+    createdAt: "2023-01-28T15:35:53.607+0000",
+  },
+  {
+    _id: "2",
+    image: images.post3,
+    title: "Help children get better education",
+    createdAt: "2023-03-05T15:35:53.607+0000",
+  },
+  {
+    _id: "3",
+    image: images.post1,
+    title: "Help children get better education",
+    createdAt: "2023-03-03T15:35:53.607+0000",
+  },
+];
+
+const tagsData = [
+  "Medical",
+  "Lifestyle",
+  "Learn",
+  "Healthy",
+  "Food",
+  "Diet",
+  "Education",
+];
 
 const ArticleDetailPage = () => {
-  const breadCrumbsData = [
-    { name: "Home", link: "/" },
-    { name: "Blog", link: "/blog" },
-    { name: "Article title", link: "/blog/1" },
-  ];
+  const { slug } = useParams();
+  const userState = useSelector((state) => state.user);
+  const [breadCrumbsData, setBreadCrumbsData] = useState([]);
+  const [body, setBody] = useState(null);
 
-  const postsData = [
-    {
-      _id: "1",
-      image: images.post2,
-      title: "Help children get better education",
-      createdAt: "2023-01-28T15:35:53.607+0000",
+  const { data, isLoading, isError } = useQuery({
+    queryFn: () => getSinglePost({ slug }),
+    queryKey: ["blog", slug],
+    onSuccess: (data) => {
+      setBreadCrumbsData([
+        { name: "Home", link: "/" },
+        { name: "Blog", link: "/blog" },
+        { name: `${data.title}`, link: `/blog/${data.slug}` },
+      ]);
+      setBody(
+        parse(
+          generateHTML(data?.body, [Bold, Italic, Text, Paragraph, Document])
+        )
+      );
     },
-    {
-      _id: "2",
-      image: images.post3,
-      title: "Help children get better education",
-      createdAt: "2023-03-05T15:35:53.607+0000",
-    },
-    {
-      _id: "3",
-      image: images.post1,
-      title: "Help children get better education",
-      createdAt: "2023-03-03T15:35:53.607+0000",
-    },
-  ];
-
-  const tagsData = [
-    "Medical",
-    "Lifestyle",
-    "Learn",
-    "Healthy",
-    "Food",
-    "Diet",
-    "Education",
-  ];
+  });
 
   return (
     <MainLayout>
-      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
-        <article className="flex-1">
-          <BreadCrumbs data={breadCrumbsData} />
-          <img className="rounded-xl w-full" src={images.post1} alt="" />
-          <Link
-            to="/blog?catagory=selectedCategory"
-            className="text-primary text-sm text-roboto inline-block mt-4 md:text-base"
-          >
-            EDUCATION
-          </Link>
-          <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
-            Help children get better education
-          </h1>
-          <div className="mt-4 text-dark-soft">
-            <p className="leading-7">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae
-              congue mauris rhoncus aenean vel elit scelerisque. In egestas erat
-              imperdiet sed euismod nisi porta lorem mollis. Morbi tristique
-              senectus et netus. Mattis pellentesque id nibh tortor id aliquet
-              lectus proin.
-            </p>
-          </div>
-          <CommentsContainer className="mt-10" logginedUserId="a" />
-        </article>
-        <div>
-          <SuggestedPosts
-            header="Latest Article"
-            posts={postsData}
-            tags={tagsData}
-            className="mt-8 lg:mt-0 lg:max-w-xs"
-          />
-          <div className="mt-7 ">
-            <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
-              Share on:
-            </h2>
-            <SocialShareButtons
-              url={encodeURI("https://google.com")}
-              title={encodeURIComponent("Selim Deneme")}
+      {isLoading ? (
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message="Couldn't fetch the post Detail" />
+      ) : (
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <article className="flex-1">
+            <BreadCrumbs data={breadCrumbsData} />
+            <img
+              className="rounded-xl w-full"
+              src={
+                data?.photo
+                  ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
+                  : images.samplePhoto
+              }
+              alt={data?.title}
             />
+            <div className="mt-4 flex gap-2">
+              {data?.categories.map((category) => (
+                <Link
+                  to={`/blog?category=${category?.name}`}
+                  className="text-primary text-sm text-roboto inline-block  md:text-base"
+                >
+                  {category?.name}
+                </Link>
+              ))}
+            </div>
+
+            <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
+              {data?.title}
+            </h1>
+            <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
+            <CommentsContainer
+              className="mt-10"
+              comments={data?.comments}
+              logginedUserId={userState?.userInfo?._id}
+              postSlug={slug}
+            />
+          </article>
+          <div>
+            <SuggestedPosts
+              header="Latest Article"
+              posts={postsData}
+              tags={tagsData}
+              className="mt-8 lg:mt-0 lg:max-w-xs"
+            />
+            <div className="mt-7 ">
+              <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
+                Share on:
+              </h2>
+              <SocialShareButtons
+                url={encodeURI("https://google.com")}
+                title={encodeURIComponent("Selim Deneme")}
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </MainLayout>
   );
 };
